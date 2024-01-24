5292587bae3fe76fad0eb4048f902a43008f5614
Benjamin Lesieux <60974622+BenjaminLesieux@users.noreply.github.com>
2024-01-24 09:52:59 +0100

    gitignore

---
+++ test-utils.ts
@@ -0,0 +1,4 @@
+export const baseURL= "https://g.hr.dmerej.info/";
+export function $(path: string) {
+    return baseURL + path;
+}
---
+++ tests/employee-management.test.ts
@@ -0,0 +1,21 @@
+import {test} from "@playwright/test";
+import {$} from "../test-utils";
+
+test.describe("Employee Management", () => {
+    test.describe("Add Employee", () => {
+        test("should add a new employee", async ({page}) => {
+            await page.goto($("add_employee"));
+        });
+        test("should not add a new employee with invalid data", async ({page}) => {
+
+        });
+        test("should not add a new employee with empty data", async ({page}) => {
+
+        });
+    });
+
+    test.describe("Edit Employee", () => {
+
+    });
+});
+
